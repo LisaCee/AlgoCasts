544cef707f720fcacd30619fc5664c1bfdab9c81
LisaCee <lisaceedesign@gmail.com>
2019-07-12 14:56:12 -0700

    integer reverse solution from video

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,8 +8,9 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
+// solution # 1
 // function reverseInt(n) {
-//   return parseInt(n.toString().split('').reverse().join(''))
+//   return parseInt(n.toString().split('').reverse().join('')) * Math.sign(n)
 // }
 
 // my solution
