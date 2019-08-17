49d81914edc927e1e236ef9bb78173090a407704
LisaCee <lisaceedesign@gmail.com>
2019-08-17 16:44:40 -0700

    added Pseudocode for each solution palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,11 +8,22 @@
 //   palindrome("abcdefg") === false
 
 // solution #1
+// built in reverse method
+// split the string into array, reverse and join
+// compare to the original
+// true if it's a palindrome (matches), false if it is different.
+
 // function palindrome(str) {
 //   return str.split('').reverse().join('') == str;
 // }
 
 // solution #2
+// split the string into an array
+// for every character in array, see if it matches the last character of array
+// keep track of index value to move from the end of the array towards the middle
+// function returns once the current letter and the opposite letter no longer match or all letters are checked
+// every returns boolean
+
 // function palindrome(str) {
 //   return str.split('').every((char, i) => {
 //     return char == str[str.length - i - 1]
@@ -20,10 +31,12 @@
 // }
 
 // my solution w/ helper function
+// recursive function to reverse string
 function reverse(str) {
     return str.length < 2 ? str :
         reverse(str.slice(1)) + str[0]
 }
+// call reverse function with string and compare it to original
 function palindrome(str) {
     return reverse(str) == str;
 }
