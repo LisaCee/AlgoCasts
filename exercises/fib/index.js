c7bd323d4009c4a6eae9366c8989acc0ab5dfc6a
LisaCee <lisaceedesign@gmail.com>
2020-01-18 14:40:59 -0800

    memoization solution helper from lecture

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -46,13 +46,19 @@
 // }
 
 function memoize(fn) {
+    // reusable cache function
     const cache = {}
     return function(...args) {
-
+        if(cache[args]) {
+            return cache[args]
+        }
+        const result = fn.apply(this, args)
+        cache[args] = result;
+        return result
     }
 }
 
-function slowFib(n) {
+function fib(n) {
 
     // recursive solution
     if (n < 2) {
