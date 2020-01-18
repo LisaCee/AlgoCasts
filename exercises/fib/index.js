3049df034a867aa956c0adccc1dae336e7013012
LisaCee <lisaceedesign@gmail.com>
2020-01-18 14:35:06 -0800

    seperate different solutions in fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,37 +8,60 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-    // iterative solution
-    // let array = [0, 1]
-    // for (let i = 1; i <= n ; i++) {
-    //     array.push(array[i-1] + array[i])
-    // }
-    // return array[n]
+// function fib(n) {
+//     // iterative solution
+//     let array = [0, 1]
+//     for (let i = 1; i <= n ; i++) {
+//         array.push(array[i-1] + array[i])
+//     }
+//     return array[n]
+// }
+
+// function fib(n) {
+//     // recursive solution
+//     if (n < 2) {
+//         return n
+//     } else {
+//         return fib(n-1) + fib(n-2) 
+//     }
+// }
+
+// function fib(n) {
+//     //my memoization fib
+//     let cache = {0:0, 1:1}
+//     let i = 2
+//     if (cache[n]) {
+//         return cache[n]
+//     } else {
+//         if (n < 2) {
+//             return n
+//         } else {
+//             while (i <= n) {
+//                 cache[i] = cache[i-1] + cache[i-2]
+//                 i++
+//             }
+//         }
+//     }
+//     return cache[n]
+// }
+
+function memoize(fn) {
+    const cache = {}
+    return function(...args) {
+
+    }
+}
+
+function slowFib(n) {
 
     // recursive solution
-    // if (n < 2) {
-    //     return n
-    // } else {
-    //     return fib(n-1) + fib(n-2) 
-    // }
-
-    //memoization
-    let cache = {0:0, 1:1}
-    let i = 2
-    if (cache[n]) {
-        return cache[n]
+    if (n < 2) {
+        return n
     } else {
-        if (n < 2) {
-            return n
-        } else {
-            while (i <= n) {
-                cache[i] = cache[i-1] + cache[i-2]
-                i++
-            }
-        }
+        return fib(n-1) + fib(n-2) 
     }
-    return cache[n]
+
+    
 }
 
 
